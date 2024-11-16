a7044ea5975bd89d152abe2e9b1648a7384bf3ca
Dani Alcubilla <86176019+dan-alcubilla@users.noreply.github.com>
2024-11-15 23:40:24 -0400

    HeaderTop finished

--- src/components/list-header.tsx
+++ src/components/list-header.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import { View, Text, StyleSheet, Image, Pressable, TouchableOpacity } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 import { FontAwesome } from '@expo/vector-icons';
@@ -16,23 +16,28 @@ export const ListHeader = () => {
                         <Text style={styles.avatarText}>Hello Alexis</Text>
                     </View>
                 </View>
-                <Link style={styles.cartContainer} href='/cart' asChild>
-                    <Pressable>
-                        {({ pressed }) => (
-                            <View>
-                                <FontAwesome
-                                    name='shopping-cart'
-                                    size={25}
-                                    color='gray'
-                                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                                />
-                                <View style={styles.badgeContainer}>
-                                    <Text style={styles.badgeText}>{1}</Text>
+                <View style={styles.headerRight}>
+                    <Link style={styles.cartContainer} href='/cart' asChild>
+                        <Pressable>
+                            {({ pressed }) => (
+                                <View>
+                                    <FontAwesome
+                                        name='shopping-cart'
+                                        size={25}
+                                        color='gray'
+                                        style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                                    />
+                                    <View style={styles.badgeContainer}>
+                                        <Text style={styles.badgeText}>{2}</Text>
+                                    </View>
                                 </View>
-                            </View>
-                        )}
-                    </Pressable>
-                </Link>
+                            )}
+                        </Pressable>
+                    </Link>
+                    <TouchableOpacity style={styles.signOutButton}>
+                        <FontAwesome name="sign-out" size={25} color='red' />
+                    </TouchableOpacity>
+                </View>
             </View>
             <View style={styles.heroContainer}></View>
             <View style={styles.categoriesContainer}></View>
